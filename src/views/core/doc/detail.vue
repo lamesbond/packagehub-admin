17bff1d9a779fda81acf5f41e37557bbea0d02cc
lamesbond <1418190515@qq.com>
2022-06-02 17:59:09 +0800

    改了些前端文件

---
+++ src/views/core/doc/detail.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app-container">
+    <h4>文档信息</h4>
+
+    <el-row style="text-align:center;margin-top: 40px;">
+      <el-button @click="back">
+        返回
+      </el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import lendApi from '@/api/core/lend'
+import '@/styles/show.css'
+export default {
+  name: "",
+  data() {
+    return {
+      lendDetail: {
+        lend: {
+          param: {}
+        },
+        borrower: {}
+      }
+    }
+  },
+
+  created() {
+    if (this.$route.params.id) {
+      this.fetchDataById()
+    }
+  },
+
+  methods: {
+    fetchDataById() {
+      lendApi.show(this.$route.params.id).then(response => {
+        this.lendDetail = response.data.lendDetail
+      })
+    },
+
+    back() {
+      this.$router.push({path: '/core/doc/list'})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
